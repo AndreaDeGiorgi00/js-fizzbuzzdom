3de2de07fa626fffea628c36272e446d08553631
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2022-12-07 16:00:05 +0100

    programma che stampa in console tutti i numeri e i fizz e Buzz

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("java OKs")
+/* Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
+ per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz” al posto del numero.
+ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.*/
+
+
+
+ // 1-ti stampa da 1 a 100 
+    for (let i=1 ; i <= 100 ; i++){
+ // 2- seleziono i diversi valori possibili
+    if(i%3==0 && i%5==0){
+        /*stampa*/
+        console.log("FizzBuzz")
+    }else if(i%3==0){
+        /*stampa*/
+        console.log("Fizz")
+    }
+    else if(i%5==0){
+        /*stampa*/
+        console.log("Buzz")
+    }else{
+        /*stampa*/
+        console.log(i)
+    }
+    
+    }
