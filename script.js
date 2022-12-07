3c1939febc38bb02cbd66004e4fa5f030c386890
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2022-12-07 16:57:14 +0100

    programma che stampa in pagina i valori da 1 a 100

--- script.js
+++ script.js
@@ -3,24 +3,27 @@
 per i multipli di 5 stampi “Buzz” al posto del numero.
  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.*/
 
-
+let targetContenitore = document.getElementById("conteniner");
 
  // 1-ti stampa da 1 a 100 
     for (let i=1 ; i <= 100 ; i++){
  // 2- seleziono i diversi valori possibili
     if(i%3==0 && i%5==0){
         /*stampa*/
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
+        targetContenitore.innerHTML += ('<div class="casella col">fizzBuzz </div>');
     }else if(i%3==0){
         /*stampa*/
-        console.log("Fizz")
+        console.log("Fizz");
+        targetContenitore.innerHTML += ('<div class="casella col">Fizz </div>');
     }
     else if(i%5==0){
         /*stampa*/
-        console.log("Buzz")
+        console.log("Buzz");
+        targetContenitore.innerHTML += ('<div class="casella col">Buzz </div>');
     }else{
         /*stampa*/
-        console.log(i)
-    }
+        targetContenitore.innerHTML += (`<div class="casella col"> ${i} </div>`);
+    };
     
-    }
+    };
